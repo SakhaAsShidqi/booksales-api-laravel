4adb3a8a2fa7cc5b07260a71f5947426fb5b6393
Sakha As Shidqi <146859332+SakhaAsShidqi@users.noreply.github.com>
2025-10-23 22:02:20 +0700

    tugas 6 framework laravel done

--- booksales-api/bootstrap/app.php
+++ booksales-api/bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\IsAdmin; // <<< Tambahkan ini
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +12,16 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        
+    
+        $middleware->alias([
+            'is.admin' => IsAdmin::class, 
+        ]);
+        
+        $middleware->validateCsrfTokens(except: [
+            'debug-api/*', 
+        ]);
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- booksales-api/routes/api.php
+++ booksales-api/routes/api.php
@@ -1,30 +1,21 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\GenreController;
 use App\Http\Controllers\AuthorController;
-use App\Http\Controllers\BookController;
-use App\Http\Controllers\SaleController;
+use App\Http\Controllers\GenreController;
 
-Route::middleware('api')->group(function () {
-    // Books
-    Route::get('books', [BookController::class, 'index']);
-    Route::get('books/{book}', [BookController::class, 'show']);
+Route::apiResource('authors', AuthorController::class)->only(['index', 'show']);
 
-    // Authors (gunakan apiResource)
-    Route::apiResource('authors', AuthorController::class);
 
-    // Genres (gunakan apiResource)
-    Route::apiResource('genres', GenreController::class);
+Route::apiResource('genres', GenreController::class)->only(['index', 'show']);
 
-    // Sales
-    Route::get('sales', [SaleController::class, 'index']);
-    Route::get('sales/{sale}', [SaleController::class, 'show']);
-    Route::post('sales', [SaleController::class, 'store']);
+Route::middleware(['auth:sanctum', 'is.admin'])->group(function () {
+
+    Route::apiResource('authors', AuthorController::class)->only(['store', 'update', 'destroy']);
+
+    Route::apiResource('genres', GenreController::class)->only(['store', 'update', 'destroy']);
 });
 
-// fallback
 Route::fallback(function () {
-    return response()->json(['status'=>'error','message'=>'API endpoint tidak ditemukan'], 404);
+    return response()->json(['status' => 'error', 'message' => 'API endpoint tidak ditemukan'], 404);
 });
